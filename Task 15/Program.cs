902b1db835ad5d7ccb52aa9bc8194fabc90de816
SilentMary <playinglassbead@gmail.com>
2023-08-18 06:49:32 +0100

    Task 15 done

--- Task 15/Program.cs
+++ Task 15/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Converts a number into a day of the week and says if it's a weekend
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <=7)
+    {
+        return true;
+    }
+    Console.WriteLine("It is not a weekday!");
+    return false;
+}
+
+int weekDay = Prompt("Input weekday: ");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Finally it's weekend!");
+    }
+    else 
+    {
+        Console.WriteLine("You gotta work bish");
+    }
+}
