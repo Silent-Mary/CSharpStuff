378dc11effe0c4d984ff8ee0ea0cb6f3bc900119
SilentMary <playinglassbead@gmail.com>
2023-08-18 10:27:12 +0100

    Solved Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Input two numbers: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine("1st number is max");
+}
+if(number2 > number1)
+{
+    Console.WriteLine("2nd number is max");
+}
+if(number1 == number2) 
+{
+    Console.WriteLine("Please pick differing integers");
+}
